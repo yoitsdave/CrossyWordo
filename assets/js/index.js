7898d1e64dfb8ba6095c27289ebc32f33cbf954d
Judah Levy <judahjlevy@gmail.com>
2018-07-30 20:54:14 -0400

    Enable Enter ButtonTo Be Used To Navigate Rooms

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,26 @@
 export var App = {
   run: function() {
-    var new_submit = document.getElementById('new-submit');
     var old_submit = document.getElementById('old-submit');
-    var new_name = document.getElementById('new-name');
+    var new_submit = document.getElementById('new-submit');
     var old_name = document.getElementById('old-name');
+    var new_name = document.getElementById('new-name');
 
-    new_name.oninput = function() { new_submit.action = "room/" + new_name.value };
     old_name.oninput = function() { old_submit.action = "room/" + old_name.value };
+    new_name.oninput = function() { new_submit.action = "room/" + new_name.value };
+
+    old_name.addEventListener("keyup", function(event) {
+      event.preventDefault();
+      if (event.keyCode === 13) {
+        document.getElementById("old-submit-btn").click();
+      }
+    });
+
+    new_name.addEventListener("keyup", function(event) {
+      event.preventDefault();
+      if (event.keyCode === 13) {
+        document.getElementById("new-submit-btn").click();
+      }
+    });
+
   }
 }
