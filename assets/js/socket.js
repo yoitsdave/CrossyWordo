7e1e5b397032aa8bcf3bbe1abfbba5ca7141e6ef
Judah Levy <judahjlevy@gmail.com>
2018-08-09 11:45:30 -0400

    Update Clue When Necessary And Add Keyboard

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -4,10 +4,26 @@ import {Socket} from "phoenix"
 
 export var App = {
   main: function() {
-    //TODO implement mobile kb
     //TODO implement checkAll button
     //TODO implement seekNextWord/seekPrevWord button
+
     //TODO implement rebus
+
+    function needsKeyboard() {
+      var check = false;
+      (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+      return check;
+    }
+
+    function forwardKeyPress(key) {
+      let e = new Event("keydown");
+      e.key = key;
+      e.keyCode = e.key.charCodeAt(0);
+      e.which = e.keyCode;
+      e.code = "Key" + key;
+      console.log(key, e.keyCode);
+      document.dispatchEvent(e);
+    }
 
     function handleBackspace() {
       let contents = document.getElementById("contents" + window.pointer)
@@ -38,8 +54,6 @@ export var App = {
     function toggleDirection() {
       if (window.direction === "down") {window.direction = "across"}
       else {window.direction = "down"}
-
-      updateClue();
     }
 
     function colorSelected() {
@@ -313,6 +327,7 @@ export var App = {
       }
       //Left Arrow
       else if (e.which == 37){
+        e.preventDefault();
         if (window.direction === "across") {seekPrev()}
         else {
           toggleDirection();
@@ -321,6 +336,7 @@ export var App = {
       }
       //Right Arrow
       else if (e.which == 39){
+        e.preventDefault();
         if (window.direction === "across") {seekNext()}
         else {
           toggleDirection();
@@ -329,6 +345,7 @@ export var App = {
       }
       //Up Arrow
       else if (e.which == 38){
+        e.preventDefault();
         if (window.direction === "down") {seekPrev()}
         else {
           toggleDirection();
@@ -337,6 +354,7 @@ export var App = {
       }
       //Down Arrow
       else if (e.which == 40){
+        e.preventDefault();
         if (window.direction === "down") {seekNext()}
         else {
           toggleDirection();
@@ -362,6 +380,8 @@ export var App = {
           seekPrevWord();
         }
       }
+
+     updateClue();
     }
 
     function removeTextVis(number) {
@@ -398,19 +418,16 @@ export var App = {
 
     function seekPrev() {
       movePointer(getPrev(window.pointer));
-      updateClue();
       colorSelected();
     }
 
     function seekNextWord() {
       movePointer(getNextWordStart(window.pointer));
-      updateClue();
       colorSelected();
     }
 
     function seekPrevWord() {
       movePointer(getPrevWordStart(window.pointer));
-      updateClue();
       colorSelected();
     }
 
@@ -426,7 +443,7 @@ export var App = {
       let map = (window.direction === "across") ? window.across_clues : window.down_clues;
 
       clue.innerText = num.toString() + ". " + map.get(num);
-      textFit(clue, {alignHoriz: true, alignVert: true, maxFontSize: 25});
+      textFit(clue, {alignHoriz: true, alignVert: true, maxFontSize: 25, minFontSize: 6});
     }
 
     function main() {
@@ -453,6 +470,22 @@ export var App = {
       window.channel.push("call_in", {call: "get_clues"});
 
       document.getElementById("board").onclick = handleClick;
+
+      if (needsKeyboard()){
+      //if (true) {
+        let oskar = require("oskar");
+
+        let keyMap = {
+          0: [
+              ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+              ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+              ['Z', 'X', 'C', 'V', 'B', 'N', 'M'],
+              [' ']
+             ]
+        }
+        let kb = oskar({"keys": keyMap, "onkeypress": forwardKeyPress});
+        kb.appendTo(document.getElementById("keyboard"));
+      }
     }
 
     main();
