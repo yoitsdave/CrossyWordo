39be610f9a36f1d4832f424def7b9d4f3b688f3f
Judah Levy <judahjlevy@gmail.com>
2018-08-12 10:28:04 -0400

    Include Distillery Version Info

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Crossywordo.Mixfile do
   def project do
     [
       app: :crossywordo,
-      version: "0.0.2",
+      version: "0.1.0",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
