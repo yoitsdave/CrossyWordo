2237ef67fa4063163d74c3e6fdf6f2502c3d9d8e
Judah Levy <judahjlevy@gmail.com>
2018-08-28 02:41:29 -0400

    Fix Friends Table

--- lib/crossywordo/repo/contact.ex
+++ lib/crossywordo/repo/contact.ex
@@ -2,12 +2,12 @@ defmodule Crossywordo.Contact do
   use Ecto.Schema
   import Ecto.Changeset
 
-
+  @primary_key false
   schema "contacts" do
     field :status, :integer
 
-    belongs_to :user, Crossywordo.User
-    belongs_to :contact, Crossywordo.User
+    belongs_to :user, Crossywordo.User, primary_key: true
+    belongs_to :contact, Crossywordo.User, primary_key: true
 
     timestamps()
   end
